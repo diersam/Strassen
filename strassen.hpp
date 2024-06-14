3245bfc96d1260a2654b6cb00e9ea5bbb0dee8e0
Henryk Laqua <hlaqua@alpha.tc.cup.uni-muenchen.de>
2024-06-14 02:53:20 +0200

    working on 2x2x2x Strassen algorithm

--- strassen.hpp
+++ strassen.hpp
@@ -8,9 +8,9 @@
 //#define NDEBUG
 
 template<typename Num>
-void matmult_strassen(BlockSparseMatrix<Num>& C, 
-             const BlockSparseMatrix<Num>& A, const bool transA,
-             const BlockSparseMatrix<Num>& B, const bool transB,
+void matmult_strassen(BlockSparseMatrix<Num>& C_mat, 
+             const BlockSparseMatrix<Num>& A_mat, const bool transA,
+             const BlockSparseMatrix<Num>& B_mat, const bool transB,
              const Num thresh, const Num& alpha, const Num& beta)
 {
   //only these options so far
@@ -25,42 +25,42 @@ void matmult_strassen(BlockSparseMatrix<Num>& C,
   if (beta == Num(1)){
     //nothing to do
   }else if (beta == Num(0)){
-    C.zero();
+    C_mat.zero();
   }else{
-    C *= beta;
+    C_mat *= beta;
   }
 
   //check dimensions
-  const size_t ni = transA? A.ncol() : A.nrow();
-  const size_t nj = transB? B.nrow() : B.ncol();
-  const size_t nk1 = transA? A.nrow() : A.ncol();
-  const size_t nk2 = transB? B.ncol() : B.nrow();
+  const size_t ni = transA? A_mat.ncol() : A_mat.nrow();
+  const size_t nj = transB? B_mat.nrow() : B_mat.ncol();
+  const size_t nk1 = transA? A_mat.nrow() : A_mat.ncol();
+  const size_t nk2 = transB? B_mat.ncol() : B_mat.nrow();
 
   assert(nk1 == nk2);
   const size_t nk = nk1;
 
-  const size_t i_block_size = transA? A.max_blocksize_col() : A.max_blocksize_row();
-  const size_t j_block_size = transB? B.max_blocksize_row() : B.max_blocksize_col();
+  const size_t i_block_size = transA? A_mat.max_blocksize_col() : A_mat.max_blocksize_row();
+  const size_t j_block_size = transB? B_mat.max_blocksize_row() : B_mat.max_blocksize_col();
 
-  assert(i_block_size == C.max_blocksize_row());
-  assert(j_block_size == C.max_blocksize_col());
-  assert(ni == C.nrow());
-  assert(nj == C.ncol());
+  assert(i_block_size == C_mat.max_blocksize_row());
+  assert(j_block_size == C_mat.max_blocksize_col());
+  assert(ni == C_mat.nrow());
+  assert(nj == C_mat.ncol());
 
-  const size_t k_block_size1 = transA? A.max_blocksize_row() : A.max_blocksize_col();
-  const size_t k_block_size2 = transB? B.max_blocksize_col() : B.max_blocksize_row();
+  const size_t k_block_size1 = transA? A_mat.max_blocksize_row() : A_mat.max_blocksize_col();
+  const size_t k_block_size2 = transB? B_mat.max_blocksize_col() : B_mat.max_blocksize_row();
   assert(k_block_size1 == k_block_size2);
   const size_t k_block_size = k_block_size1;
 
   const size_t nib = integer_division_round_up(ni,i_block_size);
-  assert(nib == C.nrowblocks());
-  assert(nib == transA? A.ncolblocks() : A.nrowblocks());
+  assert(nib == C_mat.nrowblocks());
+  assert(nib == transA? A_mat.ncolblocks() : A_mat.nrowblocks());
   const size_t njb = integer_division_round_up(nj,j_block_size);
-  assert(njb == C.ncolblocks());
-  assert(njb == transB? B.nrowblocks() : B.ncolblocks());
+  assert(njb == C_mat.ncolblocks());
+  assert(njb == transB? B_mat.nrowblocks() : B_mat.ncolblocks());
   const size_t nkb = integer_division_round_up(nk,k_block_size);
-  assert(nkb == transA? A.nrowblocks() : A.ncolblocks());
-  assert(nkb == transB? B.ncolblocks() : B.nrowblocks());
+  assert(nkb == transA? A_mat.nrowblocks() : A_mat.ncolblocks());
+  assert(nkb == transB? B_mat.ncolblocks() : B_mat.nrowblocks());
 
   //only quadratic so far
   assert(ni == nj);
@@ -77,8 +77,8 @@ void matmult_strassen(BlockSparseMatrix<Num>& C,
       const size_t ib = ijkb%nib;
       const size_t jb = (ijkb/nib)%njb;
       const size_t kb = ijkb/(nib*njb);
-      const auto& a_block = transA? A.block(kb,ib) : A.block(ib,kb);
-      const auto& b_block = transB? B.block(jb,kb) : B.block(kb,jb);
+      const auto& a_block = transA? A_mat.block(kb,ib) : A_mat.block(ib,kb);
+      const auto& b_block = transB? B_mat.block(jb,kb) : B_mat.block(kb,jb);
       const Num norm_a = a_block.frobenius_norm();
       const Num norm_b = b_block.frobenius_norm();
       const Num est = norm_a*norm_b;
@@ -93,13 +93,18 @@ void matmult_strassen(BlockSparseMatrix<Num>& C,
     const size_t n_flops_total = std::accumulate(nflops_per_level[0].cbegin(),nflops_per_level[0].cend(),0lu);
     printf("total flops on level %lu: %lu\n",0lu, n_flops_total);
   }
-  const size_t max_nflops_per_block = 2lu*i_block_size*j_block_size*k_block_size;
+  const size_t max_nflops_per_block_mult = 2lu*i_block_size*j_block_size*k_block_size;
+  const size_t max_nflops_per_block_add = i_block_size*j_block_size;
+  //additions are more expensive per op as mults, how much exactly depends on cache structure
+  std::array<double,20> Strassen_fac = {10,10,10,25,35,35,35,35,35,35,35,35,35,35,35,35,35,35,35,35}; 
   //recursive bottom up to decide between Strassen and sparse at each level iterating the block MM count upwards
   for(size_t level=0;level<max_level;++level){// bottom up
     const size_t previous_super_block_size = (size_t)std::exp2(level);//no of blocks in previous level(1,2,4...)
     const size_t super_block_size = (size_t)std::exp2(level+1);//no of blocks in super-block (2,4,8...)
-    const size_t Strassen_thresh = (size_t)std::round(std::pow(7,level+1))*max_nflops_per_block;
-    printf("level: %lu size: %lu Strassen_thresh: %lu\n",level,super_block_size,Strassen_thresh);
+    const size_t n_flops_per_strassen_mult = (size_t)std::round(std::pow(7,level+1))*max_nflops_per_block_mult;
+    const size_t n_flops_per_strassen_add_sub = (size_t)(15e0*Strassen_fac[level]*std::pow(4,level+1)*(double)max_nflops_per_block_add);//we trade 15 adds/subs for one mult
+    const size_t n_flops_per_strassen = n_flops_per_strassen_mult + n_flops_per_strassen_add_sub;
+    printf("level: %lu size: %lu n_flops_per_strassen: %lu\n",level,super_block_size,n_flops_per_strassen);
 
     const size_t n_super_i = integer_division_round_up(nib,super_block_size);
     const size_t n_super_j = integer_division_round_up(njb,super_block_size);
@@ -172,10 +177,10 @@ void matmult_strassen(BlockSparseMatrix<Num>& C,
         }
       }
       //check for sparse or Strassen
-      const bool do_Strassen = (nflops >= Strassen_thresh) && !is_edge_case;
+      const bool do_Strassen = (nflops >= n_flops_per_strassen) && !is_edge_case;
       if (do_Strassen){
         //upwards iteration of FLOPs-count
-        nflops_per_level[level+1][ijk(super_i,super_j,super_k,n_super_i,n_super_j)] = Strassen_thresh;
+        nflops_per_level[level+1][ijk(super_i,super_j,super_k,n_super_i,n_super_j)] = n_flops_per_strassen;
       }else{//we do conventional
         //upwards iteration of FLOPs-count
         nflops_per_level[level+1][ijk(super_i,super_j,super_k,n_super_i,n_super_j)] = nflops;
@@ -185,6 +190,146 @@ void matmult_strassen(BlockSparseMatrix<Num>& C,
     const size_t n_flops_total = std::accumulate(nflops_per_level[level+1].cbegin(),nflops_per_level[level+1].cend(),0lu);
     printf("total flops on level %lu: %lu\n",level+1, n_flops_total);
   }
+}
+//only for 2x2x2 block multiplications
+template<typename Num>
+void matmult_strassen_2x2x2(BlockSparseMatrix<Num>& C_mat, 
+             const BlockSparseMatrix<Num>& A_mat, const bool transA,
+             const BlockSparseMatrix<Num>& B_mat, const bool transB,
+             const Num thresh, const Num& alpha, const Num& beta)
+{
+  //only these options so far
+  assert(alpha == Num(1));
+  assert(beta == Num(1));
+
+  //no on-the-fly transformation yet
+  assert(transA == false);
+  assert(transB == false);
+
+  //using Mat = Matrix<Num,BlockAllocator<Num>>;
+  if (beta == Num(1)){
+    //nothing to do
+  }else if (beta == Num(0)){
+    C_mat.zero();
+  }else{
+    C_mat *= beta;
+  }
+
+  //check dimensions
+  const size_t ni = transA? A_mat.ncol() : A_mat.nrow();
+  const size_t nj = transB? B_mat.nrow() : B_mat.ncol();
+  const size_t nk1 = transA? A_mat.nrow() : A_mat.ncol();
+  const size_t nk2 = transB? B_mat.ncol() : B_mat.nrow();
+
+  assert(nk1 == nk2);
+  const size_t nk = nk1;
+
+  const size_t i_block_size = transA? A_mat.max_blocksize_col() : A_mat.max_blocksize_row();
+  const size_t j_block_size = transB? B_mat.max_blocksize_row() : B_mat.max_blocksize_col();
+
+  assert(i_block_size == C_mat.max_blocksize_row());
+  assert(j_block_size == C_mat.max_blocksize_col());
+  assert(ni == C_mat.nrow());
+  assert(nj == C_mat.ncol());
+
+  const size_t k_block_size1 = transA? A_mat.max_blocksize_row() : A_mat.max_blocksize_col();
+  const size_t k_block_size2 = transB? B_mat.max_blocksize_col() : B_mat.max_blocksize_row();
+  assert(k_block_size1 == k_block_size2);
+  const size_t k_block_size = k_block_size1;
+
+  const size_t nib = integer_division_round_up(ni,i_block_size);
+  assert(nib == C_mat.nrowblocks());
+  assert(nib == transA? A_mat.ncolblocks() : A_mat.nrowblocks());
+  const size_t njb = integer_division_round_up(nj,j_block_size);
+  assert(njb == C_mat.ncolblocks());
+  assert(njb == transB? B_mat.nrowblocks() : B_mat.ncolblocks());
+  const size_t nkb = integer_division_round_up(nk,k_block_size);
+  assert(nkb == transA? A_mat.nrowblocks() : A_mat.ncolblocks());
+  assert(nkb == transB? B_mat.ncolblocks() : B_mat.nrowblocks());
+
+  //only quadratic so far
+  assert(ni == nj);
+  assert(ni == nk);
+  assert(nib == njb);
+  assert(nib == nkb);
+
+  const size_t max_level = (size_t)std::log2(nib);
+  //only 2x2xx so far
+  assert(nib == 2);
+  assert(njb == 2);
+  assert(nkb == 2);
+
+  //only for "perfect" blocks
+  assert(ni % i_block_size == 0);
+  assert(nj % j_block_size == 0);
+  assert(nk % k_block_size == 0);
+  //only without on-the-fly transpose for now
+  assert(transA == false);
+  assert(transB == false);
+
+#if 1
+  //use the Winograd form
+  const auto& a = A_mat.block(0,0);
+  const auto& b = A_mat.block(0,1);
+  const auto& c = A_mat.block(1,0);
+  const auto& d = A_mat.block(1,1);
+
+  const auto& A = B_mat.block(0,0);
+  const auto& C = B_mat.block(0,1);
+  const auto& B = B_mat.block(1,0);
+  const auto& D = B_mat.block(1,1);
+
+  //using Mat = Matrix<Num,BlockAllocator<Num>>;
+  auto& t = C_mat.block(0,0);
+  matmult(t,a,false,A,false,1.e0,1.e0);
+  //t = a*A;
+
+  auto na_pc = c;
+  na_pc -= a;
+  auto pC_nD = C;
+  pC_nD -= D;
+  auto& u = C_mat.block(1,0);
+  matmult(u,na_pc,false,pC_nD,false,1.e0,1.e0);
+  //u = na_pc*pC_nD;
+
+  auto pc_pd = c;
+  pc_pd += d;
+  auto nA_pC = C;
+  nA_pC -= A;
+  auto& v = C_mat.block(0,1);
+  matmult(v,pc_pd,false,nA_pC,false,1.e0,1.e0);
+  //v = pc_pd*nA_pC;
+
+  auto na_pc_pd = pc_pd;
+  na_pc_pd -= a;
+  auto pA_nC_pD = D;
+  pA_nC_pD -= nA_pC;
+  //auto& w = C_mat.block(1,1);
+  auto w = t;
+  matmult(w,na_pc_pd,false,pA_nC_pD,false,1.e0,1.e0);
+
+  C_mat.block(1,1) = u;
+  C_mat.block(1,1) += v;
+  C_mat.block(1,1) += w;
+
+  auto pa_pb_nc_nd = b;
+  pa_pb_nc_nd -= na_pc_pd;
+  //C_mat.block(0,1) += pa_pb_nc_nd*D;
+  matmult(C_mat.block(0,1),pa_pb_nc_nd,false,D,false,1.e0,1.e0);
+  C_mat.block(0,1) += w;
+
+  auto nA_pB_pC_nD = B;
+  nA_pB_pC_nD -= pA_nC_pD;
+  //C_mat.block(1,0) += d*nA_pB_pC_nD;
+  matmult(C_mat.block(1,0),d,false,nA_pB_pC_nD,false,1.e0,1.e0);
+  C_mat.block(1,0) += w;
+
+  //C_mat.block(0,0) += b*B;
+  matmult(C_mat.block(0,0),b,false,B,false,1.e0,1.e0);
+
+#else
+  //use the original Strassen form
+#endif
 }
 #endif
 
